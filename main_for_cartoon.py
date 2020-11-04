1b97da07d32c75f7d16e1bff8beee77fef333684
Wiffzack <wiffzackius@gmail.com>
2020-11-04 22:54:28 +0100

    Update main_for_cartoon.py

--- main_for_cartoon.py
+++ main_for_cartoon.py
@@ -20,14 +20,20 @@ import struct
 import scipy
 import scipy.misc
 import scipy.cluster
+from scipy import fftpack
 import os
 import shutil, sys, os, glob, cv2
+
+import argparse
+#import matplotlib.pyplot as pyplot
+
+
 inFile = sys.argv[1]
 outFile = sys.argv[2]
 #dst = sys.argv[2]
 print (inFile)
  
-counter=30
+counter=29
 color_saturation=""
  
 NUM_CLUSTERS = 5
@@ -36,6 +42,91 @@ NUM_CLUSTERS = 5
 r=1
 g=1
 b=1
+
+image_idftm=np.array([[25,25,25],[0,0,0],[0,0,0]])
+image_idftp=np.array([[25,25,25],[0,0,0],[0,0,0]])
+image_idftp_value=np.array([[25,25,25],[0,0,0],[0,0,0]])
+
+def fix_image(image):
+	"""
+	Corrects the pixel values of an image represented as a 2d array.
+
+	image - The image to be corrected.
+	hist_equalization - True if histogram equalization should be applied.
+	"""
+	hist_equalization=False
+	# clip all negative pixel values to zero
+	image = image.clip(min=0)
+
+	# scale the pixel values so they fall within the interval [0,255]
+	image *= 255 / np.max(image)
+
+	if hist_equalization is False:
+		return image
+
+	image_histogram, bins = np.histogram(image.flatten(), 256, normed=True)
+
+	# compute the image's cumulative distribution function (CDF)
+	cdf = image_histogram.cumsum()
+
+	# normalize the image's CDF
+	cdf = 255 * cdf / cdf[-1]
+
+	# use linear interpolation of the CDF to adjust the image
+	return np.interp(image.flatten(), bins[:-1], cdf).reshape(image.shape)
+
+
+def plot_images(image_idft_mag,
+				image_idft_phase,
+				):
+	"""
+	Plots an image and the IDFT of the magnitude/phase parts of its DFT.
+
+	image - The original image.
+	image_idft_mag - The IDFT of the magnitude part of the original image's DFT.
+	image_idft_phase - The IDFT of the phase part of the original image's DFT.
+	output_image - An image on which the plot will be saved or None.
+	hist_equalization - True if histogram equalization should be applied.
+	"""
+	image_idftm = fix_image(image_idft_mag)
+
+	# high level of edge information
+	image_idftp = fix_image(image_idft_phase)
+
+
+def fft_image(input_image):
+	import matplotlib.pyplot as pyplot
+	#img_gray = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
+	# compute the DFT of the grayscale version of the input image
+	#fftpack.helper.next_fast_len(min_len)
+	
+	### improve performance much !!!
+	#image_dft = fftpack.fft(input_image, 16384)
+	
+	image_dft = np.fft.fft2(input_image)
+
+	# compute the IDFT of the magnitude of the image's DFT
+	image_dft_mag = np.abs(image_dft)
+	image_idft_mag = np.fft.ifft2(image_dft_mag).real
+
+	# compute the IDFT of the phase of the image's DFT
+	image_dft_phase = image_dft / image_dft_mag
+	image_idft_phase = np.fft.ifft2(image_dft_phase).real
+	image_idft_phase = image_idft_phase.astype(np.float32)
+	image_idftp_value = image_idft_phase
+	#print (image_idft_phase)
+	
+	figure = pyplot.figure()
+	figure.add_subplot(133).set_title("IDFT of DFT phase")
+	pyplot.imshow(fix_image(image_idft_phase), cmap="gray")
+	pyplot.axis("off")
+
+	pyplot.subplots_adjust(left=0.01, right=0.99, wspace=0.02)	
+	pyplot.show()
+	
+	plot_images(image_idft_mag,image_idft_phase)
+
+
 
 # stärke die farbe die am wenigsten vorkommt 
 # gitb dden richtigen touch
@@ -55,10 +146,14 @@ def color_content(image):
 	counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
 
 	index_max = scipy.argmax(counts)                    # find most frequent
+	index_min = scipy.argmin(counts) 
 	peak = codes[index_max]
+	least = codes[index_min]
 	colour = binascii.hexlify(bytearray(int(c) for c in peak)).decode('ascii')
+	least_colour = binascii.hexlify(bytearray(int(c) for c in least)).decode('ascii')	
 	print('most frequent is %s (#%s)' % (peak, colour))
-
+	#print ("least color")
+	#print (least_colour)
 	
 	#ImageColor.getcolor("#23a9dd", "RGB")
 	h = colour.lstrip('#')
@@ -134,6 +229,58 @@ def estimate_noise(img):
 	sigma = sigma * math.sqrt(0.5 * math.pi) / (6 * (W-2) * (H-2))
 	return sigma
 	
+def invert_color(image):
+	img_bgr = image
+	# dont forget cv use bgr
+	color = ('b', 'g', 'r') 
+	  
+	for i, col in enumerate(color): 
+		  
+		histr = cv2.calcHist([img_bgr], 
+							 [i], None, 
+							 [256], 
+							 [0, 256]) 
+		  
+	# Negate the original image 
+	img_neg = 1 - img_bgr 
+	  
+	return img_neg
+
+def derivation_fun(image,rate):
+	laplacian = cv2.Laplacian(image,cv2.CV_64F)
+	s = cv2.convertScaleAbs(laplacian)
+	laplace = s
+	#s = cv2.convertScaleAbs(s, 3, beta=0)
+
+	scale = 1
+	delta = 0
+	ddepth = cv2.CV_64F
+	src = cv2.GaussianBlur(img, (3, 3), 0)
+	src = cv2.convertScaleAbs(src)
+	gray = src
+	grad_x = cv2.Sobel(gray, ddepth, 1, 0, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+	grad_y = cv2.Sobel(gray, ddepth, 0, 1, ksize=3, scale=scale, delta=delta, borderType=cv2.BORDER_DEFAULT)
+	derivation = cv2.add(grad_x,grad_y)
+	derivation = cv2.convertScaleAbs(derivation)
+
+	mask = cv2.inRange(img, derivation, s)
+	total_derivation_org = cv2.bitwise_and(img,img, mask= mask)
+	total_derivation = cv2.bilateralFilter(total_derivation_org,12,9,7)
+	total_derivation = cv2.convertScaleAbs(total_derivation, 3, beta=0)
+	#smoth_derivation = cv2.bilateralFilter(derivation,12,9,7)
+	smoth_derivation = cv2.convertScaleAbs(derivation, 3, beta=0)
+
+	laplace = cv2.convertScaleAbs(laplace, 1.2, beta=0)
+
+	sumit = cv2.add(s, derivation)
+	# -0.1 smooth space between colors
+	# everything black here is one suspect
+	s = cv2.addWeighted(img, 1.0, total_derivation, -0.1*rate, 1)
+	s = cv2.addWeighted(s, 1.0, sumit, -0.1*rate, 1)
+	#s = cv2.addWeighted(img, 1.0, laplace, -0.1, 0)
+	#s = cv2.addWeighted(s, 1.0, smoth_derivation, -0.2, 0)
+	return s
+	
 for filename in os.listdir(inFile):
     if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".JPG"): 
 		os.chdir(inFile)
@@ -157,14 +304,36 @@ for filename in os.listdir(inFile):
 		
 		result_lv = detail_entropy/old_noise_lv
 		
+		# Try to improve edges
+		#
+		if(detail_entropy>7.5):
+			img = derivation_fun(img,0.5)
+		else:
+			img = derivation_fun(img,1)	
+		#######################
+		
+		# fft 
+		#gray_img_scp = scipy.misc.imread(filename, mode="L")
+		# god help me.
+		#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert color format form BGR to RGB (OpenCV default is BGR).
+		#img = img.astype(np.float)  # Convert ot float
+		# is equal to scipy.misc.imread
+				
 		# high entropy > 7
 		# high noise   > 3.5
 		#### high entropy and low noise  -> 1:more details
 		#### high entropy and high noise -> 1:noisy but consider level of detail
 		#### low entropy and low noise   -> 3:skip
 		#### low entropy and high noise  -> 2:noisy 
+		high_noise_tol=3.5
+		high_entropy = 7
 		
+		# useless to slow cant use the result now:
 		
+		#if(old_noise_lv>1):
+		#	fft_image(gray_img)	
+		
+	
 		############################################
 		##  the problem is we losing information....
 		##
@@ -182,39 +351,90 @@ for filename in os.listdir(inFile):
 		hColor = 13	
 	
 		#1:
-		if ((detail_entropy > 7 and old_noise_lv<3.5) or (detail_entropy > 7 and old_noise_lv>3.5)):
+		if ((detail_entropy > high_entropy and old_noise_lv<3.5) or (detail_entropy > high_entropy and old_noise_lv>3.5)):
 			d=2
-			templateWindowSize = 2
-			searchWindowSize =2
+			sigmaColor = 7
+			sigmaSpace = 7
 			#fastNlMeansDenoisingColored
 			templateWindowSize = 2
 			searchWindowSize =2
 			h = 5
 			hColor = 7	
 		#2:
-		if (detail_entropy < 7 and old_noise_lv>3.5):
+		if (detail_entropy < high_entropy and old_noise_lv>3.5):
 			d=6
-			templateWindowSize = 10
-			searchWindowSize =10
+			sigmaColor = 7
+			sigmaSpace = 7
 			#fastNlMeansDenoisingColored
 			templateWindowSize = 3
 			searchWindowSize =3
 			h = 7
 			hColor = 21			
-	
+
+		# Edge filter kernel
+		# some value between 1-9. Lower values mean less details.
+		# its more or less random
+		# for low noise image
+		filter = np.array([[-1, -1, -1], [-1, detail_entropy, -1], [-1, -1, -1]])
+		noise = np.array([[-1, -1, -1], [-1, result_lv, -1], [-1, -1, -1]])		
+		img_shapend=cv2.filter2D(img,-1,filter)
+
+		# for higher noise images
+		filter2 = np.array([[-1, -1, -1], [-1, (detail_entropy-old_noise_lv), -1], [-1, -1, -1]])
+		noise2 = np.array([[-1, -1, -1], [-1, result_lv, -1], [-1, -1, -1]])				
+		img_shapend2=cv2.filter2D(img,-1,filter)		
+		#loop counter
+		loop_counter_addweight=1
+		corr_value=0
+		fix_v=1
+		
+		#image_idftp
+		#ifft_img_org = Image.fromarray(image_idftp)
+		#ifft_img= ifft_img_org.convert('L') 
+		#ifft_img_cv = np.asarray(ifft_img)
+		
 		#3:
-		if(old_noise_lv<3 and detail_entropy < 6.7):
+		if(old_noise_lv<1):
 			pass
 		else:		
 			while True:
+				# escape endless loop
+				if(loop_counter_addweight==4):
+					break
+				
+				# Applying cv2.filter2D function
+				# % should depend of the level of detail 
+				if(old_noise_lv<high_noise_tol):
+					img = cv2.addWeighted(img,0.95+corr_value,img_shapend,0.05-corr_value,0)
+				else:
+					#adjust filter parameter :
+					d=d*fix_v
+					sigmaColor = sigmaColor*fix_v
+					sigmaSpace = sigmaSpace*fix_v
+					if(loop_counter_addweight>2):
+						templateWindowSize = templateWindowSize +3 
+						searchWindowSize =searchWindowSize + 3
+						h = h+7
+						hColor = hColor+21	
+					#noise_remover = cv2.filter2D(img, -1, noise2)
+					img = cv2.addWeighted(img,0.95+corr_value,img_shapend2,0.05-corr_value,0)
+					#img = cv2.subtract(img,noise_remover)
+				##################################################		
 				img = cv2.bilateralFilter(img, d, sigmaColor, sigmaSpace)
-				img = cv2.fastNlMeansDenoisingColored(img, None, templateWindowSize, searchWindowSize, h, hColor)	
+				img = cv2.fastNlMeansDenoisingColored(img, None, templateWindowSize, searchWindowSize, h, hColor)
 				gray_img  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 				new_noise_lv = estimate_noise(gray_img)
+				# get negative if noise is adde via cvtColor
 				noise_diff = old_noise_lv - new_noise_lv
 				old_noise_lv = new_noise_lv
 				print (new_noise_lv)
-				if(noise_diff<0.5):
+				# <0.1 detect to small changes 
+				if(noise_diff<0):
+					corr_value=corr_value+0.01
+					fix_v=1+fix_v
+				else:
+					loop_counter_addweight=loop_counter_addweight+1
+				if(abs(noise_diff)>0.4 or abs(noise_diff)<0.2):
 					break
 				#templateWindowSize= 2*templateWindowSize
 			
@@ -233,7 +453,7 @@ for filename in os.listdir(inFile):
 				break
 		
 		
-		img = cv2.convertScaleAbs(img, alpha=alpha, beta=beta)
+		#img = cv2.convertScaleAbs(img, alpha=alpha, beta=beta)
 		img = change_brightness(img, value=-20)
 		img = Image.fromarray(img)
 		###################################################
@@ -254,7 +474,7 @@ for filename in os.listdir(inFile):
 		print(brightness_value)
 		color_saturation = color_content(img)
 		
-		too_bright = 190
+		too_bright = 180
 		if (brightness_value> too_bright and color_saturation != "white"):
 			enhancer = ImageEnhance.Brightness(img)
 			new_fac = 1-(too_bright/brightness_value)
